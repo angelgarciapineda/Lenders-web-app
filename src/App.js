f047656871d35999ccf9b43525e2137761966e63
Luis Angel Garcia Pineda <garciapinedaluisangel@gmail.com>
2021-07-11 22:59:36 -0500

    Navegacion entre paginas del sitio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import Login from "./components/login/login";
 import SignUp from "./components/login/register";
-import Home from "./components/home/home";
+//import Home from "./components/home/home";
+import NavBar from "./components/nav-bar/NavBar";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import "./App.css";
 function App() {
   return (
-    <Router>
-      <Link to="/">Iniciar sesion</Link>
-      <Link to="/signup">Registrarse</Link>
-      <Link to="/home">Home</Link>
-      <Switch>
-        <Route path="/" exact>
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        {/* <Link to="/">Iniciar sesion</Link>
+        <Link to="/signup">Registrarse</Link>
+        <Link to="/home">Home</Link> */}
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+        </Switch>
+        {/* <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch> */}
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Products.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Products() {
+  return <div>Productos</div>;
+}
+
+export default Products;
