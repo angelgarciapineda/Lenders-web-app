53fc52e827bb6da7d173e38a8c33bafa985be563
EmmanuelBazan <mqkz99@gmail.com>
2021-08-01 22:27:58 -0500

    recuperar productos de bd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../styles/home.css";
 import ProductCard from "../components/product-card/product-card";
 import { FiSearch } from "react-icons/fi"
+import axios from 'axios';
+import cors from 'cors';
+
+const producto = {
+  nom_producto: "Bicicleta de montania",
+  descripcion: "Bicicleta de montaña Schwinn dos meses de uso, seminueva",
+  fotografia: "https://img.icons8.com/cotton/2x/image--v2.png",
+  catalogo: "PRODUCTO",
+  ofertador: "Mario gonzales",
+  id_usuario: 0,
+  id_producto: 0,
+  id_catalogo: 0
+}
+
+const URL = "http://localhost:3000/vistaProductos"
 
 function Home() {
+
+  const arrayProductos = useRef([])
+
+  useEffect(() => {
+    let unmounted = false;
+
+    if(!unmounted){
+
+      (async () => {
+        try {
+
+          await axios.get(URL)
+          .then(res => {
+            
+            arrayProductos.current = res.data
+            console.log("PRODUCTOS -------------------> ",arrayProductos.current);
+            
+          })
+
+        }
+        catch (error) {
+          console.log("ERROR AL RECUPERAR PRODUCTOS ------------------> ",error);
+        }
+      })();
+
+    }
+
+    return () => {unmounted = true}
+  }, [])
+
   return <div className="generalContainer">
     <div className="divSearch">
       <input className="inputSearch"/>
@@ -12,14 +57,24 @@ function Home() {
       </div>
     </div>
     <div className="divProducts">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {
+        arrayProductos.current.map(element => {
+
+          const current = {
+            nom_producto: element.nom_producto,
+            descripcion: element.descripcion,
+            fotografia: element.fotografia,
+            catalogo: element.categoria,
+            ofertador: element.nombre +" "+ element.apellidoP,
+            id_producto: element.id_producto
+          }
+
+          return (
+            <ProductCard producto={current} key={element.id_producto}/>
+          )
+
+        })
+      }
     </div>
   </div>;
 }
